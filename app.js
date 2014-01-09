2f8c9b4dcf29d843fb3d4029cbe8937dcf94b7d0
Bryan Gamotea <bryan.gamotea26@gmail.com>
2014-01-09 10:57:06 +0800

    Edited.

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 $(document).ready(function (){
-	$('.home').mouseenter(function(){
-		$('home').animate({opacity:'1'},1000);
-	});
+	// $('.home').mouseenter(function(){
+	// 	$('.home').css({'opacity':'1'});
+	// });
+	// $('.home').mouseleave(function(){
+	// 	$('.home').css({'opacity':'.25'});
+	// });
+	function onMouseLeave(){
+		$(this).mouseleave(function(){
+			$(this).css({'opacity':'.25'});
+		});
+	}
+
+		function onMouseEnter(){
+		$(this).mouseenter(function(){
+			$(this).css({'opacity':'1'});
+		});
+	}
+
+	$('.home').mouseenter(onMouseEnter);
+	$('.home').mouseleave(onMouseLeave);
+	$('.history').mouseenter(onMouseEnter);
+	$('.history').mouseleave(onMouseLeave);
 });
